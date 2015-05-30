cab6c8eb28de75ee3e542c71fb61340e879cfb1a
Chris Bohl <chrisrbohl@gmail.com>
2015-05-30 12:11:11 -0500

    create dog views

--- app/assets/javascripts/backbone/init.js
+++ app/assets/javascripts/backbone/init.js
@@ -1,10 +1,13 @@
+//This is specific for the dashboard. It will eventually need its own file, or we will need to add routes.
+
 $(document).ready( function(){
   var user = new User({id: 1});
+
+  var dogsView = new DogsView({collection: user.dogs})
+
   user.dogs.fetch({
     success: function(){
       console.log("Found dogs!");
     }
-  }).done(function(){
-    // debugger;
   });
 });
---
+++ app/assets/javascripts/backbone/views/dogs_view.js
@@ -0,0 +1,34 @@
+var DogsView = Backbone.View.extend({
+  template: JST["backbone/templates/dashboard_dogs"],
+  el: "#dogs",
+
+  initialize: function(){
+    console.log("Creating dogs view!");
+    console.log(this.collection);
+    this.listenTo(this.collection, 'reset', this.addAll);
+  },
+
+  render: function() {
+    this.$el.html(this.template());
+
+    return this;
+  },
+
+  addOne: function() {
+    var view = new DogsView({model: dog});
+    view.render();
+
+    this.$el.find(".dogs").append(view.el);
+
+    return this;
+  },
+
+  addAll: function() {
+    this.collection.each(function(dog){
+      this.addOne(dog);
+    }, this);
+
+    return this;
+  }
+
+});
